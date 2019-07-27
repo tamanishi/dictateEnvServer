442c88f68d7ccd5a166ad1826b9da969daa09919
Masayuki Sunahara <tamanishi@gmail.com>
2019-07-28 00:14:40 +0900

    maintenance for RaspberryPi

--- server.js
+++ server.js
@@ -1,47 +1,38 @@
 require('dotenv').config();
 var express = require('express');
 var googlehome = require('google-home-notifier');
-// var ngrok = require('ngrok');
 var bodyParser = require('body-parser');
 var app = express();
 const serverPort = 8080;
-var exec = require('child_process').exec
+var exec = require('child_process').exec;
 
 var deviceName = 'Google Home';
 googlehome.device(deviceName, 'ja');
-// googlehome.accent('uk'); // uncomment for british voice
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 let cmd = process.env.CMD;
 
-app.post('/google-home-notifier', urlencodedParser, function (req, res) {
-  if (!req.body) return res.sendStatus(400)
-  console.log(req.body);
-  var text = req.body.text;
-  if (text){
-    try {
-      googlehome.notify(text, function(notifyRes) {
-        console.log(notifyRes);
-        res.send(deviceName + ' will say: ' + text + '\n');
-        exec(cmd, (err, stdout, stderr) => {
-          if(err){
-            console.log(err);
-          }
-          console.log(stdout);
-        });
-      });
-    } catch(err) {
-      console.log(err);
-      res.sendStatus(500);
-      res.send(err);
-    }
-  }else{
-    res.send('Please POST');
+app.post('/google-home-notifier', urlencodedParser, (req, res) => {
+  try {
+    exec(cmd, (err, stdout, stderr) => {
+      let text;
+      if(err){
+        text = "温湿度情報の計測に失敗しました。";
+      } else {
+        let envObj = JSON.parse(stdout);
+        text = "現在の温度は" + envObj.t + "度。湿度は" + envObj.h + "パーセント。気圧は" + envObj.p + "ヘクトパスカルです。";
+      }
+      googlehome.notify(text, () => {});
+    });
+  } catch(err) {
+    let text = "何かしらエラーが発生しました。";
+    console.log(err);
+    res.sendStatus(500);
+    res.send(err);
+    googlehome.notify(text, () => {});
   }
-
-})
-
-app.listen(serverPort, function () {
-  console.log('curl -X POST -d "text=Hello World" http://localhost:' + serverPort + '/google-home-notifier');
 });
+
+app.listen(serverPort);
+
