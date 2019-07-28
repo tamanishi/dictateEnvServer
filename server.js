fab7fd1246bd4270ae163c6e863c9d4f1922395c
Masayuki Sunahara <tamanishi@gmail.com>
2019-07-28 09:57:01 +0900

    return response tweak dotenv

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+const path = require('path');
+const ENV_PATH = path.join(__dirname, '.env');
+require('dotenv').config({path: ENV_PATH});
 var express = require('express');
 var googlehome = require('google-home-notifier');
 var bodyParser = require('body-parser');
@@ -15,6 +17,8 @@ let cmd = process.env.CMD;
 
 app.post('/google-home-notifier', urlencodedParser, (req, res) => {
   try {
+//    console.log("ENV_PATH : " + ENV_PATH);
+//    console.log("cmd : " + cmd);
     exec(cmd, (err, stdout, stderr) => {
       let text;
       if(err){
@@ -24,6 +28,8 @@ app.post('/google-home-notifier', urlencodedParser, (req, res) => {
         text = "現在の温度は" + envObj.t + "度。湿度は" + envObj.h + "パーセント。気圧は" + envObj.p + "ヘクトパスカルです。";
       }
       googlehome.notify(text, () => {});
+      res.sendStatus(200);
+      res.send();
     });
   } catch(err) {
     let text = "何かしらエラーが発生しました。";
