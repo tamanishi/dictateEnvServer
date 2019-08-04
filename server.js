af193357844bcfcc60fc932d3a19f3a9eff87c58
Masayuki Sunahara <tamanishi@gmail.com>
2019-08-04 09:52:46 +0900

    merge voicetext branch

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 const path = require('path');
 const ENV_PATH = path.join(__dirname, '.env');
 require('dotenv').config({path: ENV_PATH});
+var fs = require('fs');
 var express = require('express');
 var googlehome = require('google-home-notifier');
 var bodyParser = require('body-parser');
 var app = express();
 const serverPort = 8080;
 var exec = require('child_process').exec;
+let Voicetext = require('voicetext');
+var voice = new Voicetext(process.env.VOICETEXT_APIKEY);
 
-var deviceName = 'Google Home';
+let deviceName = 'Google Home';
 googlehome.device(deviceName, 'ja');
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 let cmd = process.env.CMD;
+let serverUrl = process.env.SERVERURL;
+
+app.use(express.static(path.join(__dirname, '/audio')));
 
 app.post('/google-home-notifier', urlencodedParser, (req, res) => {
   try {
-//    console.log("ENV_PATH : " + ENV_PATH);
-//    console.log("cmd : " + cmd);
     exec(cmd, (err, stdout, stderr) => {
       let text;
       if(err){
@@ -27,13 +31,22 @@ app.post('/google-home-notifier', urlencodedParser, (req, res) => {
         let envObj = JSON.parse(stdout);
         text = "現在の温度は" + envObj.t + "度。湿度は" + envObj.h + "パーセント。気圧は" + envObj.p + "ヘクトパスカルです。";
       }
-      googlehome.notify(text, () => {});
+      voice
+      .speaker(voice.SPEAKER.HIKARI)
+      .speak(text, (err, buf) => {
+        if(err)console.error(err);
+        fs.writeFile(__dirname + '/audio/text.wav', buf, 'binary', (err) => {
+          if(err)console.log(err);
+        });
+      });
+//      googlehome.notify(text, () => {});
+      googlehome.play(serverUrl + '/text.wav', () => {});
       res.status(200);
       res.send(text);
     });
   } catch(err) {
     let text = "何かしらエラーが発生しました。";
-    console.log(err);
+    console.error(err);
     res.status(500);
     res.send(err);
     googlehome.notify(text, () => {});
--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 . ${SCRIPT_DIR}/.env
 
 sudo -u pi /usr/bin/node /usr/bin/forever start -a -d /home/pi/src/github.com/tamanishi/dictateEnvServer/server.js
-autossh -f -M 0 -N -R ${SUBDOMAIN}:80:localhost:8080 serveo.net
+autossh -f -M 0 -N -R ${SERVEO_SUBDOMAIN}:80:localhost:8080 serveo.net
 
