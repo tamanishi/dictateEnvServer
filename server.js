ff30efd0522735b4db12addd6b818d82d503d77a
Masayuki Sunahara <tamanishi@gmail.com>
2019-08-04 11:28:58 +0900

    wav -> mp3 accepts "speaker" parameter

--- server.js
+++ server.js
@@ -31,16 +31,19 @@ app.post('/google-home-notifier', urlencodedParser, (req, res) => {
         let envObj = JSON.parse(stdout);
         text = "現在の温度は" + envObj.t + "度。湿度は" + envObj.h + "パーセント。気圧は" + envObj.p + "ヘクトパスカルです。";
       }
+      let speaker = (req.body.speaker === undefined ? voice.SPEAKER.HIKARI : req.body.speaker);
       voice
-      .speaker(voice.SPEAKER.HIKARI)
+      .speaker(speaker)
+      .format(voice.FORMAT.MP3)
+      .emotion(voice.EMOTION.HAPINESS)
       .speak(text, (err, buf) => {
         if(err)console.error(err);
-        fs.writeFile(__dirname + '/audio/text.wav', buf, 'binary', (err) => {
+        fs.writeFile(__dirname + '/audio/text.mp3', buf, 'binary', (err) => {
           if(err)console.log(err);
-        });
+          googlehome.play(serverUrl + '/text.mp3', () => {});
+       });
       });
 //      googlehome.notify(text, () => {});
-      googlehome.play(serverUrl + '/text.wav', () => {});
       res.status(200);
       res.send(text);
     });
